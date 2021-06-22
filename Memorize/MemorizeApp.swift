a6c1fb0c25233d60f7735ffdf132e3c1a0382227
atorresruiz8 <atorresruiz8@gmail.com>
2021-06-22 07:52:08 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Antonio Torres-Ruiz on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
